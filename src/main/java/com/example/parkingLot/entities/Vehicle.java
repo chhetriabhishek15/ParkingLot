cebe0b7e8eae040414c51599c5496827103ec2b3
Abhishek <Abhishek.Chhetri@coforge.com>
2025-10-22 14:30:10 +0530

    added all entities and enums - till date

--- src/main/java/com/example/parkingLot/entities/Vehicle.java
+++ src/main/java/com/example/parkingLot/entities/Vehicle.java
@@ -2,6 +2,8 @@ package com.example.parkingLot.entities;
 
 import com.example.parkingLot.enums.VeichleType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -10,12 +12,19 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "veichles")
+@Table(name = "vehicles",
+indexes = @Index(name = "idx_vehicle_type", columnList = "vehicleType")
+)
 public class Veichle {
     @Id
+    @NotBlank
     private String veichleNumber;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private VeichleType veichleType;
+
+    @OneToMany(mappedBy = "veichle",cascade = {CascadeType.PERSIST, CascadeType.MERGE},fetch = FetchType.LAZY)
+    private List<Ticket> ticketList;
 }
